257fb6fbdbb40acd423322ee2c2e34aabebb244f
SampurnaNiyogi <sampurna.niyogi06@gmail.com>
2025-10-31 14:24:59 +0530

    Updated Doubly Linked List

--- DoublyLinkedList.c
+++ DoublyLinkedList.c
@@ -6,69 +6,79 @@ struct node{
     struct node *next;
 };
 
-void insertAtBeginning(struct node **pointerToHead, int val){
+void insertAtBeginning(struct node **pointerToHead, struct node **pointerToTail, int val){
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = val;
     newNode->prev = NULL;
     if (*pointerToHead == NULL){
         newNode->next = NULL;
         *pointerToHead = newNode;
+        *pointerToTail = newNode;
         return;
     }
     newNode->next = *pointerToHead;
+    (*pointerToHead)->prev = newNode;
     *pointerToHead = newNode;
     return;
 }
 
-void insertAtEnd(struct node **pointerToHead, int val){
+void insertAtEnd(struct node **pointerToHead, struct node **pointerToTail, int val){
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = val;
     newNode->next = NULL;
     if (*pointerToHead == NULL){
         newNode->prev = NULL;
         *pointerToHead = newNode;
+        *pointerToTail = newNode;
         return;
     }
-    struct node *curr = *pointerToHead;
-    while (curr->next != NULL){
-        curr = curr->next;
-    }
-    curr->next = newNode;
-    newNode->prev = curr;
+    (*pointerToTail)->next = newNode;
+    newNode->prev = *pointerToTail;
+    *pointerToTail = newNode;
 }
 
-void deleteAtBeginning(struct node **pointerTohead){
+void deleteAtBeginning(struct node **pointerTohead, struct node **pointerToTail){
     if (*pointerTohead == NULL){
         return;
     }
     struct node *deletedNode = *pointerTohead;
+    if (*pointerTohead == *pointerToTail){
+        *pointerTohead = NULL;
+        *pointerToTail = NULL;
+        free(deletedNode);
+        return;
+    }
     *pointerTohead = (*pointerTohead)->next;
     (*pointerTohead)->prev = NULL;
     free(deletedNode);
     return;
 }
 
-void deleteAtEnd(struct node **pointerTohead){
+void deleteAtEnd(struct node **pointerTohead, struct node **pointerToTail){
     if (*pointerTohead == NULL){
         return;
     }
-    struct node *curr = *pointerTohead;
-    while (curr->next != NULL){
-        curr = curr->next;
+    struct node *deletedNode = *pointerTohead;
+    if (*pointerTohead == *pointerToTail){
+        *pointerTohead = NULL;
+        *pointerToTail = NULL;
+        free(deletedNode);
+        return;
     }
-    struct node *deletedNode = curr;
-    curr->prev->next = NULL;
+    *pointerToTail = (*pointerToTail)->prev;
+    (*pointerToTail)->next = NULL;
     free(deletedNode);
+    return;
 }
 
-void traverseList(struct node *head){
+void traverseList(struct node *head, struct node *tail){
     if (head == NULL){
         printf("Linked List is empty : ");
         return;
     }
     struct node *ptr = head;
     printf("Linked List : ");
-    while (ptr != NULL){
+    while (ptr != tail->next){
         printf("%d <-> ", ptr->data);
         ptr = ptr->next;
     }
@@ -76,8 +86,8 @@ void traverseList(struct node *head){
 }
 
 int main(){
-    struct node *head = (struct node *)malloc(sizeof(struct node));
-    head = NULL;
+    struct node *head = NULL;
+    struct node *tail = NULL;
     int ch, val;
     while (1)
     {
@@ -89,22 +99,22 @@ int main(){
         case 1:
             printf("Enter the value to be inserted: ");
             scanf("%d",&val);
-            insertAtBeginning(&head, val);
-            traverseList(head);
+            insertAtBeginning(&head, &tail, val);
+            traverseList(head, tail);
             break;
         case 2:
             printf("Enter the value to be inserted: ");
             scanf("%d",&val);
-            insertAtEnd(&head, val);
-            traverseList(head);
+            insertAtEnd(&head, &tail, val);
+            traverseList(head, tail);
             break;
         case 3:
-            deleteAtBeginning(&head);
-            traverseList(head);
+            deleteAtBeginning(&head, &tail);
+            traverseList(head, tail);
             break;
         case 4:
-            deleteAtEnd(&head);
-            traverseList(head);
+            deleteAtEnd(&head, &tail);
+            traverseList(head, tail);
             break;
         case 5:
             exit(0);
